87526a8d2da78359c82203cfa1c08364a55825e1
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-11-22 19:43:11 +0530

    frontend basic functionality done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Tracker from "./components/Tracker";
+
+function App() {
+	return (
+		<>
+			<h1>Hi there</h1>
+			<Tracker />
+		</>
+	);
+}
+
+export default App;
---
+++ client/src/components/Tracker.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+function Tracker() {
+	const [expenses, setExpenses] = useState([]);
+	const [name, setName] = useState("");
+	const [desc, setDesc] = useState("");
+	const [amount, setAmount] = useState(0);
+
+	const handleAddExpense = (e) => {
+		e.preventDefault();
+		setExpenses((prevExpenses) => [
+			...prevExpenses,
+			{
+				name,
+				desc,
+				amount,
+			},
+		]);
+		setName("");
+		setDesc("");
+		setAmount("");
+	
+	};
+
+	return (
+		<>
+			<h1>Inside Tracker</h1>
+
+			<ul>
+				{expenses.length <= 0 ? (
+					<p>NO expenses</p>
+				) : (
+					expenses.map((exp) => (
+						<li key={exp.name}>
+							{exp.name} - {exp.desc} - Rs. {exp.amount}
+						</li>
+                    ))
+				)}
+			</ul>
+			
+			<form onSubmit={handleAddExpense}>
+				<input
+					type="text"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<input
+					type="text"
+					value={desc}
+					onChange={(e) => setDesc(e.target.value)}
+				/>
+				<input
+					type="number"
+					value={amount}
+					onChange={(e) => setAmount(e.target.value)}
+				/>
+				<button>Add Expenses</button>
+			</form>
+		</>
+	);
+}
+
+export default Tracker;
