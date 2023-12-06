f611ea853e9722dfb03cd929c0430dd62dc12400
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-12-06 16:32:33 +0530

    routes added

---
+++ server/routes/expense.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const expenseController = require("../controller/expense");
+
+router.get("/", expenseController.getExpenses);
+
+router.post("/", expenseController.postExpenses);
