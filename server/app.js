7e14d6211fa8a07d9d3c34eca71864d86c631ecf
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-12-06 16:36:11 +0530

    router+db synched

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyparser = require('body-parser');
+const expenseRoutes = require('./routes/expense')
+const sequelize = require('./util/database')
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req,res,next) => {
-    res.send('<h1>Working</h1>')
+app.use(bodyparser.json());
+
+sequelize.sync({force : true}).then(() => {
+    console.log('DB synched');
 })
+
+app.use('/expenses', expenseRoutes)
+
 app.listen(PORT, () => {
 	console.log(`Server is running at ${PORT}`);
 });
